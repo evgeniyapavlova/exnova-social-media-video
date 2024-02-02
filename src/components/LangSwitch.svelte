1f41c4dfc42c78320086cbe9f8b6bf910b779882
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2024-02-02 16:52:47 +0300

    Start project

---
+++ src/components/LangSwitch.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	const options: Record<string, { key: string; name: string }> = {
+		en: { key: 'en', name: 'Eng' },
+		ar: { key: 'ar', name: 'Ar' }
+	};
+	export let value = options.en.key;
+	let checked = true;
+
+	/**
+	 * @param {{ target: any; }} event
+	 */
+	function handleClick(event: MouseEvent) {
+		const target = event.target as HTMLButtonElement;
+
+		const state = target.getAttribute('aria-checked');
+
+		checked = state === 'true' ? false : true;
+
+		value = checked === true ? options.en.key : options.ar.key;
+	}
+</script>
+
+<div class="switch-lang__container">
+	<button role="switch" aria-checked={checked} on:click={handleClick}>{options[value].name}</button>
+</div>
+
+<style>
+	.switch-lang__container {
+		width: 160px;
+		height: 40px;
+		background-color: var(--color-switch);
+		border-radius: 10px;
+		padding: 4px;
+		position: relative;
+	}
+
+	.switch-lang__container button {
+		background-color: #fff;
+		border: none;
+		border-radius: 8px;
+		font-weight: bold;
+		text-align: center;
+		width: 50%;
+		height: calc(100% - 8px);
+		position: absolute;
+		left: 4px;
+		transition: left 0.2s ease-in-out;
+		cursor: pointer;
+	}
+
+	.switch-lang__container button[aria-checked='true'] {
+		left: 4px;
+	}
+
+	.switch-lang__container button[aria-checked='false'] {
+		left: calc(50% - 4px);
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<!-- <footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer> -->
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
